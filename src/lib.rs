0b2158403e61beeaec8be718559569aca78f29a9
Ben Adams <darwinzero@gmail.com>
2023-07-03 19:46:31 +1200

    add packages

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![doc = include_str!("../Readme.md")]
 
-#[cfg(feature = "fixed")]
-pub use secret_data_tools_fixed as fixed;
+#[cfg(feature = "dp")]
+pub use secret_data_tools_dp as dp;
+
+#[cfg(feature = "spatial")]
+pub use secret_data_tools_spatial as spatial;
